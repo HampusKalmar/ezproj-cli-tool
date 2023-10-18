60d9b028fceb96625f1e5cd4e9d8d9b0b872c87a
Hampus Tuisku <ht222mi@student.lnu.se>
2023-10-18 13:26:01 +0200

    Remove arguments in main function

--- distribution/ezproj.bat
+++ distribution/ezproj.bat
@@ -1,5 +1,3 @@
 @echo off
 set SCRIPT_DIR=%~dp0
-echo Running from: "%SCRIPT_DIR%"
-echo Executing command: java -jar "%SCRIPT_DIR%ezproj-all.jar" %*
 java -jar "%SCRIPT_DIR%ezproj-all.jar" %*
--- src/main/java/ezprojCLI/EzProj.java
+++ src/main/java/ezprojCLI/EzProj.java
@@ -1,19 +1,10 @@
 package ezprojCLI;
 
-import java.util.Arrays;
-
 import picocli.CommandLine;
 
 public class EzProj {
 
   public static void main(String[] args) {
-    if (args.length == 0 || !"ezproj".equals(args[0])) {
-      System.out.println("Error: the ezproj command is required");
-      System.exit(1);
-    }
-
-    args = Arrays.copyOfRange(args, 1, args.length);
-
     int exitCode = new CommandLine(new EzprojCommand()).execute(args);
     System.exit(exitCode);
   }
